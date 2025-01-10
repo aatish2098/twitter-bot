125c33f77c2959ab9005ea1285fef4b17a54c6e4
aatish2098 <aatish2098@gmail.com>
2025-01-10 06:07:16 -0500

    Added topics.py

--- Scrappon.py
+++ Scrappon.py
@@ -1,10 +1,13 @@
 import asyncio
+import os
 import re
 
 import tweety.exceptions
 from qdrant_client.http import models as qdrant_models
 from tweety import TwitterAsync
 from pymongo import MongoClient
+from topics import topics
+from tweety.constants import HOME_TIMELINE_TYPE_FOR_YOU, HOME_TIMELINE_TYPE_FOLLOWING
 from tweety.filters import SearchFilters
 
 from usernames import usernames  # Your array of usernames
@@ -21,38 +24,55 @@ async def fetch_and_store_tweets():
     db = client['Knowledge']
     tweets_collection = db.OldTweets
     app = TwitterAsync("session")
-    await app.sign_in("great_o1d", "Application@2025")
-    for target_username in usernames:
-        try:
-            user = await app.get_user_info(target_username)
-            # Get up to 15 "pages" of tweets (scraping logic)
-            all_tweets = await app.get_user_media(user, 6,2)
-            # Process each tweet
-            for tweet in all_tweets:
-                # Fetch full tweet text
-                print(tweet)
-                tweet_detail = await app.tweet_detail(f"https://twitter.com/{target_username}/status/{tweet.id}")
-                tweet_text = tweet_detail._get_tweet_text()
-                cleaned_text = clean_tweet_text(tweet_text)
+    await app.sign_in(os.environ['username'], os.environ['password'])
+    trendings=await app.get_trends()
+    tweets = await app.get_home_timeline(timeline_type=HOME_TIMELINE_TYPE_FOLLOWING,pages=3)
+    for trending in trendings:
+        if trending._get_name() in topics:
+            print(trending._get_name())
+    for tweet in tweets:
+        tweet_detail = await app.tweet_detail(f"https://twitter.com/{tweet.author.username}/status/{tweet.id}")
+        print(tweet_detail.text)
+        for topic in topics:
+            if topic.lower() in tweet_detail.lower():
+                print("tweet to him", tweet_detail.text)
 
-                # Check if tweet has more than 3 words
-                if len(cleaned_text.split()) > 3:
-                    # Prepare doc for MongoDB
-                    doc = {
-                        "tweet_id": str(tweet.id),
-                        "username": target_username,
-                        "tweet_text": cleaned_text,
-                        "created_on": tweet_detail.created_on
-                    }
-                    # Insert into MongoDB
-                    tweets_collection.insert_one(doc)
 
-                    print(f"Stored tweet from {target_username}: {cleaned_text[:60]}...")
-        except tweety.exceptions.RateLimitReached:
-            print("Rate limit reached, wait now")
-            await asyncio.sleep(900)  # Wait 15 minutes
-        except Exception as e:
-            print("Some other exception",e)
+    #tweets = app.get_home_timeline(timeline_type=HOME_TIMELINE_TYPE_FOLLOWING)
+    #for tweet in tweets:
+    #    print(tweet)
+    # for target_username in usernames:
+    #     try:
+    #         user = await app.get_user_info(target_username)
+    #         # Get up to 15 "pages" of tweets (scraping logic)
+    #         all_tweets = await app.get_user_media(user, 6,2)
+    #         # Process each tweet
+    #
+    #         for tweet in all_tweets:
+    #             # Fetch full tweet text
+    #             print(tweet)
+    #             tweet_detail = await app.tweet_detail(f"https://twitter.com/{target_username}/status/{tweet.id}")
+    #             tweet_text = tweet_detail._get_tweet_text()
+    #             cleaned_text = clean_tweet_text(tweet_text)
+    #
+    #             # Check if tweet has more than 3 words
+    #             if len(cleaned_text.split()) > 3:
+    #                 # Prepare doc for MongoDB
+    #                 doc = {
+    #                     "tweet_id": str(tweet.id),
+    #                     "username": target_username,
+    #                     "tweet_text": cleaned_text,
+    #                     "created_on": tweet_detail.created_on
+    #                 }
+    #                 # Insert into MongoDB
+    #                 tweets_collection.insert_one(doc)
+    #
+    #                 print(f"Stored tweet from {target_username}: {cleaned_text[:60]}...")
+    #     except tweety.exceptions.RateLimitReached:
+    #         print("Rate limit reached, wait now")
+    #         await asyncio.sleep(900)  # Wait 15 minutes
+    #     except Exception as e:
+    #         print("Some other exception",e)
 
 
 # MAIN ENTRY POINT
